fa1a8f1902a8815782c54f1a7d13c30d87a8b811
Dmitrii <bedmvi@protonmail.com>
2025-10-03 19:52:02 +0300

    test: add more various cases

--- utils_test.go
+++ utils_test.go
@@ -9,6 +9,9 @@ func TestReverse(t *testing.T) {
 		want string
 	}{
 		{name: "standart-1", data: "abc", want: "cba"},
+		{name: "phrase", data: "not yours", want: "sruoy ton"},
+		{name: "phrase-with-utf8", data: "Hello, 世界", want: "界世 ,olleH"},
+		{name: "spaces", data: "   jl   l", want: "l   lj   "},
 	}
 
 	for _, test := range tests {
@@ -28,6 +31,11 @@ func TestIsPalindrome(t *testing.T) {
 		want bool
 	}{
 		{name: "standart-1", data: "aba", want: true},
+		{name: "standart-2", data: "abb", want: false},
+		{name: "diff case", data: "Madam", want: true},
+		{name: "spaces&case", data: "Was it a car or a cat I saw", want: true},
+		{name: "punctuation+spaces+case", data: "A man, a plan, a canal: Panama", want: true},
+		{name: "emoji", data: "😂😊😂", want: true},
 	}
 
 	for _, test := range tests {
@@ -35,6 +43,37 @@ func TestIsPalindrome(t *testing.T) {
 			got := IsPalindrome(test.data)
 			if test.want != got {
 				t.Fatalf("want -> %v got -> %v", test.want, got)
+			}
+		})
+	}
+}
+
+func TestSerialize(t *testing.T) {
+	type obj struct {
+		Num    int `json:"num"`
+		str    string
+		arrnum []int
+		arrstr []string
+	}
+
+	js := JSONSerializer{}
+
+	tests := []struct {
+		name string
+		data any
+		want []byte
+	}{
+		{name: "standart-1", data: obj{Num: 234}, want: []byte(`{"num":234}`)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := js.Serialize(test.data)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(test.want) != string(got) {
+				t.Fatalf("want -> %s got -> %s", test.want, got)
 			}
 		})
 	}
